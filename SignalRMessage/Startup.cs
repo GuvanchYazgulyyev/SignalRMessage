81d7e50647d2dae1d190710ab39e5d05d2579454
Kıvanç <guvanchy@arkadas.com.tr>
2022-12-22 17:40:47 +0300

    Dk 54

--- SignalRMessage/Startup.cs
+++ SignalRMessage/Startup.cs
@@ -30,12 +30,13 @@ namespace SignalRMessage
             .AllowAnyHeader()
             .AllowCredentials().SetIsOriginAllowed(origin => true)));
 
+           // services.AddTransient<ChatHub>();
+
             services.AddSignalR();
-            services.AddControllers();
-            services.AddRazorPages();
+            //services.AddControllers();
+            //services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,7 +46,6 @@ namespace SignalRMessage
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -60,7 +60,7 @@ namespace SignalRMessage
             {
                 //https://localhost:5001
                 endpoints.MapHub<ChatHub>("/chathub");
-                endpoints.MapControllers();
+                //endpoints.MapControllers();
             });
         }
     }
